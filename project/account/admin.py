db134e3e5f9d4e59ab797cdbd6ca17ec57e21aa4
Berkay Cakmak <brkayckmak@gmail.com>
2023-11-18 18:13:34 +0300

    account model admin panel changes

--- project/account/admin.py
+++ project/account/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from account.models import custom_user_model
 
-# Register your models here.
+class custom_admin(UserAdmin):
+    model = custom_user_model
+    list_display = ("username", "email")
+    fieldsets = UserAdmin.fieldsets + (("Avatar Change", {"fields":["avatar"]}),)
+
+admin.site.register(custom_user_model, custom_admin)
