3b362a835795d41d91198ef6ae1c23bda3fe8199
Berkay Cakmak <brkayckmak@gmail.com>
2023-11-18 17:24:00 +0300

    writing model admin page updated.

--- project/blog/admin.py
+++ project/blog/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
-from blog.models import category_model
+from blog.models import category_model, writings_model
 
 admin.site.register(category_model)
+
+class writings_admin(admin.ModelAdmin):
+    search_fields = ("title", "content")
+    list_display = ("title", "creation_date", "update_date")
+
+admin.site.register(writings_model, writings_admin)
