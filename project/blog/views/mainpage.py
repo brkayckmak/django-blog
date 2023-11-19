fa777aa00c6c2b0409c48d5d30058c7b95853c3e
Berkay Cakmak <brkayckmak@gmail.com>
2023-11-19 16:12:49 +0300

    writing pagination

--- project/blog/views/mainpage.py
+++ project/blog/views/mainpage.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+from blog.models import writings_model
+from django.core.paginator import Paginator
 
 def mainpage(request):
-    context = {"name": "Berkay Cakmak"}
+    writings = writings_model.objects.order_by("-id")
+    page = request.GET.get("page")
+
+    paginator = Paginator(writings, 1)
+    
+    context = {"writings": paginator.get_page(page)}
     return render(request, "pages/mainpage.html", context=context)
