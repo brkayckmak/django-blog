69a93a3b9d2ba3678e5cfbd2cab37968b81c585b
Berkay Cakmak <brkayckmak@gmail.com>
2023-11-19 16:43:20 +0300

    navbar fixed.

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from blog.views import contact, mainpage
+from blog.views import contact, mainpage, category
 
 urlpatterns = [
     path("", mainpage, name="mainpage"),
     path("contact/", contact, name="contact"),
+    path("category/<slug:category_slug>", category, name="category")
 ]
---
+++ project/blog/views/category.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, get_object_or_404
+from blog.models import category_model
+from django.core.paginator import Paginator
+
+def category(request, category_slug):
+    category = get_object_or_404(category_model, slug=category_slug)
+    writings = category.writing.order_by("-id")
+    
+    page = request.GET.get("page")
+    paginator = Paginator(writings, 1)
+    
+    context = {"writings": paginator.get_page(page),
+               "category_name": category.name}
+    return render(request, "pages/category.html", context=context)
