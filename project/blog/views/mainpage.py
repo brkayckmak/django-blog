d46e2c19a727ec567d980e3d197f26ab904a62e0
Berkay Cakmak <brkayckmak@gmail.com>
2023-11-20 17:47:53 +0300

    search bar fixed.

--- project/blog/views/mainpage.py
+++ project/blog/views/mainpage.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from blog.models import writings_model
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 def mainpage(request):
+    search = request.GET.get("search")
     writings = writings_model.objects.order_by("-id")
+    if search:
+        writings = writings.filter(Q(title__icontains=search) | Q(content__icontains=search)).distinct()
     page = request.GET.get("page")
 
     paginator = Paginator(writings, 1)
